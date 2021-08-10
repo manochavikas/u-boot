582ff9aaefc41ae6b562c2801b2818f43311de8b
Vikas Manocha <vikas.manocha@st.com>
2021-08-09 19:42:34 -0500

    S40: enable debugging at uart8
    Signed-off-by: Vikas Manocha <vikas.manocha@st.com>

--- board/st/stm32mp1/spl.c
+++ board/st/stm32mp1/spl.c
@@ -32,20 +32,19 @@ int board_early_init_f(void)
 #ifdef CONFIG_DEBUG_UART_BOARD_INIT
 void board_debug_uart_init(void)
 {
-#if (CONFIG_DEBUG_UART_BASE == STM32_UART4_BASE)
-
+#if (CONFIG_DEBUG_UART_BASE == STM32_UART8_BASE)
 #define RCC_MP_APB1ENSETR (STM32_RCC_BASE + 0x0A00)
 #define RCC_MP_AHB4ENSETR (STM32_RCC_BASE + 0x0A28)
 
-	/* UART4 clock enable */
-	setbits_le32(RCC_MP_APB1ENSETR, BIT(16));
+	/* UART8 clock enable */
+	setbits_le32(RCC_MP_APB1ENSETR, BIT(19));
 
-#define GPIOG_BASE 0x50008000
+#define GPIOE_BASE 0x50006000
 	/* GPIOG clock enable */
-	writel(BIT(6), RCC_MP_AHB4ENSETR);
+	writel(BIT(4), RCC_MP_AHB4ENSETR);
 	/* GPIO configuration for ST boards: Uart4 TX = G11 */
-	writel(0xffbfffff, GPIOG_BASE + 0x00);
-	writel(0x00006000, GPIOG_BASE + 0x24);
+	writel(0xfffffffb, GPIOE_BASE + 0x00);
+	writel(0x000000080, GPIOE_BASE + 0x24);
 #else
 
 #error("CONFIG_DEBUG_UART_BASE: not supported value")
